ed558227375a93d49ab98a99e5808bfc118218a9
Gerion Herbst <gherbst@3pc.de>
2018-04-16 14:54:39 +0200

    feat: Add delay to Loading Indicator

--- frontend/src/components/LoadingIndicator/LoadingIndicator.js
+++ frontend/src/components/LoadingIndicator/LoadingIndicator.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './styles.css';
 
-const LoadingIndicator = () => (
-  <div className="loading-indicator">
-    <div className="loading-indicator__spinner" />
-  </div>
-);
+class LoadingIndicator extends React.Component {
+  state = {
+    show: false,
+  };
+
+  componentDidMount() {
+    this.timer = setTimeout(() => {
+      this.setState({
+        show: true,
+      });
+    }, 500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.show ? (
+          <div className="loading-indicator">
+            <div className="loading-indicator__spinner" />
+          </div>
+        ) : null}
+      </React.Fragment>
+    );
+  }
+}
 
 export default LoadingIndicator;
